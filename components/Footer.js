5554d4d8d8c35f0bc6bc496b5bce8664aa96c423
SI <>
2023-09-04 13:47:02 +0900

    Part2 footer from components

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+        footer test from components
+      </p>
+    </footer>
+  )
+}
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
-import {Footer} from '../components/Footer'
+import { Footer } from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,12 +12,12 @@ export default function Home() {
     >
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <Head>
-          <title>Index Page</title>
+          <title>About Page</title>
           <link rel="icon" href="/favicon.ico"></link>
         </Head>
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
+          <code className="font-mono font-bold">pages/about.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -118,7 +118,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-        <Footer />
+        <Footer/>
       </div>
     </main>
   )
