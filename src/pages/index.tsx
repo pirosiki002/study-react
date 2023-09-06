d1833f0c79991b1c12d0c5dbb492c7a2ed40088c
SI <>
2023-09-06 11:40:53 +0900

    ⚗️ onclick event study

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,32 @@
 import Head from "next/head";
 import { Footer } from "src/components/Footer";
-import {Main} from "src/components/Main";
+import { Main } from "src/components/Main";
 import classes from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick =useCallback((e)=>{
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div className={classes.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <Main page = "index"/>
+      <a
+        href="/about"
+        onClick={handleClick}
+      >
+        Button
+      </a>
+      <Main page="index" />
       <Footer />
     </div>
   );
