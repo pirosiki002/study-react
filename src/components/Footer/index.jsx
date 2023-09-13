fb4e173a9392ef93c31d098dea6c8a0a8ac24e84
SI <>
2023-09-13 14:43:45 +0900

    ✨ prettier setting

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,9 @@
-import classes from "src/components/Footer/Footer.module.css"
+import classes from 'src/components/Footer/Footer.module.css';
 
 export const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-        footer test from components
-      </p>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>footer test from components</p>
     </footer>
-  )
-}
+  );
+};
